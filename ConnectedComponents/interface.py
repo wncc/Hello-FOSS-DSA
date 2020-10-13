aafe8732f7d6086e7624e24702a4263bc76f6125
sheelfshah <sheelfshah@gmail.com>
2020-10-13 17:41:26 +0530

    coreection made

--- ConnectedComponents/interface.py
+++ ConnectedComponents/interface.py
@@ -1,54 +1,25 @@
-import sys
-sys.setrecursionlimit(10**5)
-
-
 class Graph:
 
     def __init__(self, n):
-        self.connections = {}
-        self.num_vertices = n
-        self.visited = [False] * n
-        for i in range(0, n):
-            self.connections[i] = []
+        # create Graph of n nodes [1, n]
+        raise NotImplementedError
 
     def insert_edge(self, i, j):
-        self.connections[i].append(j)
-        self.connections[j].append(i)
-
-    def visit_dfs(self, v, already_connected):
-        self.visited[v] = True
-        already_connected.append(v)
-
-        for neighbor in self.connections[v]:
-            if not self.visited[neighbor]:
-                # append visited nodes to already_connected
-                already_connected = self.visit_dfs(neighbor, already_connected)
-        return already_connected
+        # insert edge between i to j (undirected)
+        raise NotImplementedError
 
     def connected_components(self):
-        ans = 0
-        for v in range(self.num_vertices):
-            if not self.visited[v]:
-                self.visit_dfs(v, [])
-                ans += 1
-        return ans
-
-
-def my_split(string):
-    return string.split()
+        # return number of connected components
+        raise NotImplementedError
 
 
 def main():
     f = open("test.in", "r")
-    t = int(f.readline())
-    for _ in range(t):
-        n, m = list(map(int, f.readline().split()))
-        g = Graph(n)
-        for _ in range(m):
-            u, v = list(map(int, f.readline().split()))
-            g.insert_edge(u - 1, v - 1)
-        ans = g.connected_components()
-        print(ans)
+    # handle input here from f
+    graph = Graph()
+
+    # handle operations and print to terminal
+
     f.close()
 
 if __name__ == '__main__':
